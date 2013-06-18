c1a21cab52fc3e35d474b50f70d3555cd4ff51c0
Sandro Bonazzola <sbonazzo@redhat.com>
2013-06-18 09:19:04 +0200

    config: added backward compatibilty
    Added backward compatibility supporting upgrade from
    3.1 version which used the keyword rhevm instead of engine.
    When reading configurarion files, the user will be warned
    that there is a deprecated key that needs to be updated.
    
    Change-Id: I6755c1ad020ee3220aa5f5335c6d66ead5139e57
    Bug-Url: https://bugzilla.redhat.com/975209
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- src/__main__.py
+++ src/__main__.py
@@ -258,6 +258,25 @@ class Configuration(dict):
         cp = ConfigParser.ConfigParser()
         cp.read(configs)
 
+        #backward compatibility with existing setup
+        if cp.has_option('ISOUploader', 'rhevm'):
+            if not cp.has_option('ISOUploader', 'engine'):
+                cp.set(
+                    'ISOUploader',
+                    'engine',
+                    cp.get('ISOUploader', 'rhevm')
+                )
+                logging.warning(
+                    _(
+                        'A deprecated configuration key has been found. '
+                        'Please replace the deprecated key, \'rhevm\', '
+                        'with the new one \'engine\' in {configFiles}'
+                    ).format(
+                        configFiles=', '.join(configs)
+                    )
+                )
+            cp.remove_option('ISOUploader', 'rhevm')
+
         # we want the items from the ISOUploader section only
         try:
             opts = [
