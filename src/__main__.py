c97c0f0a0c8617042698665ec98a39ad89938647
Sandro Bonazzola <sbonazzo@redhat.com>
2013-07-29 12:31:53 +0200

    upload: nfs: set correct umask while renaming
    set the correct umask while renaming the temporary
    file to destination file. Avoid to have incorrect
    access bits on systems with not standard umask
    (like 077)
    
    Change-Id: I5b7401f91e5fb4ce604ff17209e7c1260dd57503
    Bug-Url: https://bugzilla.redhat.com/988146
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- src/__main__.py
+++ src/__main__.py
@@ -746,7 +746,7 @@ class ISOUploader(object):
         """
         retVal = True
         logging.debug("euid(%s) egid(%s)" % (os.geteuid(), os.getegid()))
-        umask_save = os.umask(0137)  # Set to 660
+        umask_save = os.umask(0137)  # Set to 640
         try:
             src = open(src_file_name, 'r')
             os.setegid(gid)
@@ -773,6 +773,7 @@ class ISOUploader(object):
         NFS mount.
         """
         logging.debug("euid(%s) egid(%s)" % (os.geteuid(), os.getegid()))
+        umask_save = os.umask(0137)  # Set to 640
         try:
             os.setegid(gid)
             os.seteuid(uid)
@@ -783,6 +784,7 @@ class ISOUploader(object):
         finally:
             os.seteuid(0)
             os.setegid(0)
+            os.umask(umask_save)
 
     def rename_file_ssh(self, user, address, src_file_name, dest_file_name):
         """
