acda3f407d13cc518f87a3d8a87ef86c165e876c
Sandro Bonazzola <sbonazzo@redhat.com>
2013-11-15 16:45:48 +0100

    UX: remove alarming warning
    After upgrading from 3.1 users was warned about a legacy config
    still present in conf files if the user forgot to update .conf file
    with the one provided by the package as .rpmnew.
    The tool is able to use the legacy configuration so no need to
    alarm the user about that.
    
    Change-Id: I6ad7695a30f133fd1171c8dae8977ef92ea65be7
    Bug-Url: https://bugzilla.redhat.com/1031092
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- src/__main__.py
+++ src/__main__.py
@@ -287,15 +287,6 @@ class Configuration(dict):
                     'engine',
                     cp.get('ISOUploader', 'rhevm')
                 )
-                logging.warning(
-                    _(
-                        'A deprecated configuration key has been found. '
-                        'Please replace the deprecated key, \'rhevm\', '
-                        'with the new one \'engine\' in {configFiles}'
-                    ).format(
-                        configFiles=', '.join(configs)
-                    )
-                )
             cp.remove_option('ISOUploader', 'rhevm')
 
         # we want the items from the ISOUploader section only
